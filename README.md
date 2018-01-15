9feae5498c93e4127abce5b9d610f2d9c8ae3f61
Rafael Cabañas de Paz <rafacabanas@gmail.com>
2018-01-15 17:27:54 +0100

    submission

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 # Concept Drift Detection in Discrete Streaming Data Using Probabilistic Graphical Models
 
-## R. Cabañas, A. Cano, M. Gómez-Olmedo, S. Moral, A. Masegosa
+## R. Cabañas, A. Cano, M. Gómez-Olmedo,  A. Masegosa, S. Moral
 
-*Paper submitted to BNAIC 2017 (under review)*
+*Paper submitted to IPMU 2018 (under review)*
 
 This repository contains the required material for the experimentation given in the paper.  In particular, it is organised as follows:
 
@@ -12,7 +12,42 @@ This repository contains the required material for the experimentation given in 
 
 
  - ![](https://raw.githubusercontent.com/rcabanasdepaz/files/master/img/gitfolder_small.png) **datasets**:  this folder contains the data sets used in the experimentation (i.e., the synthetic and intrusion data sets) which are provided in multiple .arff files. 
- - ![](https://raw.githubusercontent.com/rcabanasdepaz/files/master/img/gitfolder_small.png) **java**: maven project for running the experiments. This uses AMIDST 0.6.2. The code implementing the concept drift detector is given in this folder.
+ - ![](https://raw.githubusercontent.com/rcabanasdepaz/files/master/img/gitfolder_small.png) **java**: maven project for running the experiments. This uses AMIDST 0.6.2. The code implementing the concept drift detector is given in this folder. For easily running the experiments shown in the paper, see the section below.
+ 
+ 
+ ## Running the paper experiments
+ 
+ 
+First, download the example project code:
+
+```bash
+$ git clone https://github.com/PGMLabSpain/2017-CDdiscrete-Code.git
+```
+
+Enter in the downloaded java folder:
+
+```bash
+$ cd 2017-CDdiscrete-Code/java/
+```
+
+The code for running the experiments is in the file **./src/main/java/BasicExample.java**. You can modify it as you want.
+
+Compile and build the package:
+
+```bash
+$ mvn clean package
+```
+
+Finally, run the code example previously mentioned:
+
+```bash
+$ java -cp target/example-project-full.jar ipmu_run
+
+```
+
+
+*Requirements: git, maven, Java8*
+
 
 
  
---
+++ java/src/main/java/ipmu_run.java
@@ -0,0 +1,444 @@
+import eu.amidst.core.datastream.DataInstance;
+import eu.amidst.core.datastream.DataOnMemory;
+import eu.amidst.core.datastream.DataStream;
+import eu.amidst.core.inference.InferenceEngine;
+import eu.amidst.core.io.DataStreamLoader;
+import eu.amidst.core.variables.Variable;
+import eu.amidst.latentvariablemodels.staticmodels.Model;
+import eu.amidst.latentvariablemodels.staticmodels.classifiers.NaiveBayesClassifier;
+import eu.amidst.rlink.PlotSeries;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.REngineException;
+
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Created by rcabanas on 23/08/2017.
+ */
+public class ipmu_run {
+
+
+
+
+
+
+	public static void main(String[] args) throws Exception {
+
+
+
+		//// Synthetic - Concept Drift detection ////
+
+		Properties synthetic = new Properties();
+		synthetic.classPresent = false;
+		synthetic.linksFromClass = false;
+		synthetic.windowSize = 1000;
+		synthetic.filename = "../datasets/syntheticNoClass_2k_1.arff";
+		synthetic.nfiles = 6;
+
+		synthetic.map = Arrays.asList(
+				new int[][]{
+						new int[]{0},
+						new int[]{1},
+						new int[]{0},
+						new int[]{},
+						new int[]{1},
+						new int[]{1},
+						new int[]{},
+				}
+		);
+
+
+
+		//// Synthetic - Severe Concept Drift detection ////
+
+		Properties syntheticSev = new Properties();
+		syntheticSev.classPresent = false;
+		syntheticSev.linksFromClass = false;
+		syntheticSev.windowSize = 1000;
+		syntheticSev.filename = "../datasets/syntheticNoClass_2k_1.arff";
+		syntheticSev.nfiles = 6;
+
+		syntheticSev.map = Arrays.asList(
+				new int[][]{
+						new int[]{0},
+						new int[]{0},
+						new int[]{0},
+						new int[]{0},
+						new int[]{0},
+						new int[]{0},
+						new int[]{0},
+				}
+		);
+
+
+
+
+
+
+		/// Synthetic - Naive Bayes Learning ////
+
+
+		Properties syntheticNB = new Properties();
+		syntheticNB.classPresent = true;
+		syntheticNB.linksFromClass = true;
+		syntheticNB.windowSize = 1000;
+		syntheticNB.filename = "../datasets/syntheticDiscrete.arff";
+		syntheticNB.nfiles = 1;
+		syntheticNB.update = false;
+
+
+		//// intrussion - Concept Drift detection ////
+
+		Properties intrussion = new Properties();
+		intrussion.classPresent = false;
+		intrussion.linksFromClass = false;
+		intrussion.windowSize = 1000;
+		intrussion.filename = "../datasets/kddcupNoClass_25k_1.arff";
+		intrussion.nfiles = 20;
+
+
+		intrussion.map = IntStream.range(0,80) // 0,79
+						.mapToObj( i ->  new int[]{} )
+						.collect(Collectors.toList());
+
+
+
+
+		intrussion.map.set(0, new int[]{0});
+		intrussion.map.set(2, new int[]{0});
+		intrussion.map.set(3, new int[]{0});
+
+
+		intrussion.map.set(71, new int[]{1});
+		intrussion.map.set(75, new int[]{1});
+
+
+
+		//// intrussion - Concept Drift detection ////
+
+		Properties intrussionSev = new Properties();
+		intrussionSev.classPresent = false;
+		intrussionSev.linksFromClass = false;
+		intrussionSev.windowSize = 1000;
+		intrussionSev.filename = "../datasets/kddcupNoClass_25k_1.arff";
+		intrussionSev.nfiles = 20;
+		intrussionSev.debug = true;
+
+
+		intrussionSev.map = IntStream.range(0,80)
+				.mapToObj( i ->  new int[]{} )
+				.collect(Collectors.toList());
+
+
+
+		IntStream.range(0,80).forEach(
+			i -> intrussionSev.map.set(i, new int[]{0})
+		);
+
+
+
+
+
+		/// intrussion - Naive Bayes Learning ////
+
+
+		Properties intrussionNB = new Properties();
+		intrussionNB.classPresent = true;
+		intrussionNB.linksFromClass = true;
+		intrussionNB.windowSize = 1000;
+		intrussionNB.filename = "../datasets/kddcup_discrete.arff";
+		intrussionNB.nfiles = 1;
+		intrussionNB.update = true;
+
+
+
+
+
+		/// Invoke experiments
+
+		run(synthetic);
+		run(intrussion);
+
+		// probabilities computation
+		computeNaiveBayes(syntheticNB);
+		computeNaiveBayes(intrussionNB);
+
+
+	run(intrussionSev);
+
+
+	}
+
+
+
+
+	private static void run(Properties p) throws REngineException {
+
+
+		/// Experiment configuration ///
+
+		String filename = p.filename;
+		int windowSize = p.windowSize;
+		boolean classPresent = p.classPresent;
+		boolean linksFromClass = p.linksFromClass;
+		int nfiles = p.nfiles;
+
+
+
+		List<int[]> map = p.map;
+
+
+
+		// Run experiment
+
+		DataStream<DataInstance> data = DataStreamLoader.open(filename);
+
+		System.out.println(data.getAttributes().toString());
+
+
+		int numHidden = map.stream().mapToInt(
+				v -> {
+					OptionalInt max = Arrays.stream(v).max();
+
+					if(max.isPresent())
+						return max.getAsInt();
+					return 0;
+				}).max().getAsInt() + 1;
+
+
+		System.out.println(numHidden);
+
+		//Build the model
+		Model model =
+				new ConceptDriftDiscrete(data.getAttributes())
+						.setWindowSize(windowSize)
+						//.setClassIndex(3)
+						.setTransitionVariance(0.1)
+						.setNumHidden(numHidden)
+						.setMapIndx(map)
+						.setLinksFromClass(linksFromClass)
+						.setClassPresent(classPresent);
+
+
+		if(p.debug)
+			System.out.println(model.getDAG());
+
+
+
+
+		List<List<Double>> series = new ArrayList<List<Double>>();
+		//	int numHidden = ((ConceptDriftDiscrete)model).getNumHidden();
+
+
+		for(int i = 0; i<numHidden; i++) {
+			series.add(new ArrayList<Double>());
+		}
+
+
+		int Nbatches = 0;
+
+		for(int f=1; f<=nfiles; f++) {
+
+			if (f > 1) {
+				filename = filename.replace((f - 1) + ".arff", f + ".arff");
+				data = DataStreamLoader.open(filename);
+			}
+
+			System.out.println(filename);
+
+			for (DataOnMemory<DataInstance> batch : data.iterableOverBatches(windowSize)) {
+				model.updateModel(batch);
+
+
+				double[] localHidenMeans = ((ConceptDriftDiscrete) model).getLocalHidenMeans();
+				System.out.println(Arrays.toString(localHidenMeans)
+						.replace("[", "")
+						.replace("]", "")
+						.replace(",", "\t"));
+
+
+				for (int i = 0; i < numHidden; i++) {
+					series.get(i).add(localHidenMeans[i]);
+				}
+
+				Nbatches++;
+				System.out.println("batch" + Nbatches);
+
+			}
+
+		}
+
+
+		//Plot....
+
+		PlotSeries plotSeries = new PlotSeries()
+				.setPlotParams("main='main title', xlab='x-axis label', ylab='y-axis label'");
+
+
+		for(int i = 0; i<numHidden; i++) {
+
+			double[] Yi = series.get(i).stream().mapToDouble(D -> D).toArray();
+
+			plotSeries.addSeries(Yi);
+		}
+
+
+		System.out.println(plotSeries.getAsignCode());
+
+
+
+	}
+
+
+
+	public static void computeNaiveBayes(Properties p) throws REngineException, REXPMismatchException {
+
+		String filename = p.filename;
+		int nfiles = p.nfiles;
+		boolean update = p.update;
+		int windowSize = p.windowSize;
+
+
+
+		DataStream<DataInstance> data = DataStreamLoader.open(filename);
+
+
+
+
+
+		System.out.println(data.getAttributes().toString());
+
+		int classIndex = data.getAttributes().getNumberOfAttributes() - 1;
+		String className = data.getAttributes().getFullListOfAttributes().get(classIndex).getName();
+
+		NaiveBayesClassifier model;
+
+		List<List<Double>> series = new ArrayList<List<Double>>();
+
+
+		int numSeries =
+				data.getAttributes().getFullListOfAttributes()
+						.stream()
+						.mapToInt(a -> {
+							int d = 0;
+							if(a.getName() != className){
+								d = a.getNumberOfStates();
+							}
+							System.out.println(a.getName()+" -> "+d);
+							return d;
+						}).sum();
+
+		for(int i = 0; i<numSeries; i++) {
+			series.add(new ArrayList<Double>());
+		}
+		int Nbatches = 0;
+
+
+		model = null;
+
+		if(update)
+			model = new NaiveBayesClassifier(data.getAttributes())
+					.setClassName(className)
+					.setWindowSize(windowSize);
+
+
+		for(int f=1; f<=nfiles; f++) {
+
+			if(f>1) {
+				filename = filename.replace((f-1)+".arff", f+".arff");
+				data = DataStreamLoader.open(filename);
+			}
+
+			System.out.println(filename);
+
+
+			for (DataOnMemory<DataInstance> batch : data.iterableOverBatches(windowSize)) {
+
+				if (!update)
+					model = new NaiveBayesClassifier(data.getAttributes())
+							.setClassName(className)
+							.setWindowSize(windowSize);
+
+
+				model.updateModel(batch);
+
+
+				Nbatches++;
+				System.out.println("batch " + Nbatches);
+
+				System.out.print(".");
+				//System.out.println(model.getModel());
+
+				int i = 0;
+				for (Variable v : model.getDAG().getVariables().getListOfVariables()) {
+					if (v != model.getClassVar()) {
+						double[] pr = InferenceEngine.getPosterior(v, model.getModel()).getParameters();
+						//	Doubles.asList(p).forEach(d -> System.out.print(d));
+						//	System.out.println();
+						for (int j = 0; j < pr.length; j++) {
+							series.get(i).add(pr[j]);
+							i++;
+						}
+
+					}
+				}
+			}
+		}
+
+		PlotSeries plotSeries = new PlotSeries()
+				.setPlotParams("main='main title', xlab='x-axis label', ylab='y-axis label'");
+
+
+		for(int i = 0; i<numSeries; i++) {
+
+			double[] Yi = series.get(i).stream().mapToDouble(D -> D).toArray();
+
+			plotSeries.addSeries(Yi);
+		}
+
+
+		System.out.println(plotSeries.getAsignCode());
+
+
+		plotSeries.toPDF("./plotNB.pdf");
+
+
+		System.out.println(Nbatches+" batches");
+
+
+
+	}
+
+
+
+
+	private static class Properties{
+
+		Properties() {
+
+		}
+
+		String filename = "./datasets/DriftSets/syntheticNoClass_2k_1.arff";
+		int windowSize = 1000;
+		boolean classPresent = true;
+		boolean linksFromClass = true;
+		int nfiles = 1;
+
+		List<int[]> map = null;
+
+		boolean update = true;
+		boolean debug = false;
+
+
+
+	}
+
+
+}
